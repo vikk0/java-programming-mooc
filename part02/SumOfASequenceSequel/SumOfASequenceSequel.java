1cfe0f88e4e5e66c2110014857653450b1862e2b
vikk0 <viktorstoyanov04@gmail.com>
2026-08-10 22:28:04 +0300

    solution: part 2 sum of a sequence sequel

---
+++ part02/SumOfASequenceSequel/SumOfASequenceSequel.java
@@ -0,0 +1,21 @@
+package SumOfASequenceSequel;
+
+import java.util.Scanner;
+
+class SumOfASequenceSequel {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("First number?");
+        int numFirst = Integer.valueOf(scanner.nextLine());
+        System.out.println("Last number?");
+        int numLast = Integer.valueOf(scanner.nextLine());
+        int sum = 0;
+
+        while (numFirst <= numLast) {
+            sum += numFirst;
+            numFirst++;
+        }
+
+        System.out.println("The sum is " + sum);
+    }
+}
