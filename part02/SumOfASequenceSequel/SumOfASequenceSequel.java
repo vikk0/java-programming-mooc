a5c5de80f05301d7233671cc8791c0de0995b411
vikk0 <viktorstoyanov04@gmail.com>
2026-08-10 22:31:38 +0300

    solution: part 2 sum of a sequence sequel

--- part02/SumOfASequenceSequel/SumOfASequenceSequel.java
+++ part02/SumOfASequenceSequel/SumOfASequenceSequel.java
@@ -5,10 +5,13 @@ import java.util.Scanner;
 class SumOfASequenceSequel {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("First number?");
         int numFirst = Integer.valueOf(scanner.nextLine());
+
         System.out.println("Last number?");
         int numLast = Integer.valueOf(scanner.nextLine());
+
         int sum = 0;
 
         while (numFirst <= numLast) {
